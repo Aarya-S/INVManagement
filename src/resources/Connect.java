007df89db237f0e6013a4f2748f95eae9abb1d18
Aarya <67827863+Aarya-S@users.noreply.github.com>
2021-08-24 13:01:24 +0530

    Update Connect.java

--- src/resources/Connect.java
+++ src/resources/Connect.java
@@ -8,7 +8,7 @@ public class Connect {
     public static Connection ConnectDB() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/invmanagement", "root", "A@rya1412");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/invmanagement", "root", "password");
             return con;
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
